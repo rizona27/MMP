941159e6d847797d7020b1f00b2961ad5c2e08f0
rizona27 <rizona.cn@gmail.com>
2025-08-31 17:22:13 +0800

    1.5.3

--- MMP/config/AboutView.swift
+++ MMP/config/AboutView.swift
@@ -63,7 +63,7 @@ struct AboutView: View {
                             .font(.largeTitle)
                             .fontWeight(.bold)
                             .foregroundColor(Color(hex: "FFD700"))
-                        Text("Version: 1.5.1      By: rizona.cn@gmail.com")
+                        Text("Version: 1.5.3      By: rizona.cn@gmail.com")
                             .font(.subheadline)
                             .foregroundColor(.secondary)
                     }
